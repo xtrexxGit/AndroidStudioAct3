047a953e964f98c2574548b326cb81896405b50e
Joeloideeeeeee <a20joelazbec@inspedralbes.cat>
2021-12-14 20:45:47 +0100

    BASE
    NO HAY NA

---
+++ Proyecto/app/src/main/java/cat/inspedralbes/damvi/m8/uf1/conversor/hodeibadiolajoellazaro/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.inspedralbes.damvi.m8.uf1.conversor.hodeibadiolajoellazaro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
